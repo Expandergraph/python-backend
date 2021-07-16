fc76eb43fe76333b6001b7b938f24c9816d7529e
r <russell.mzone@gmail.com>
2021-07-15 22:38:26 -0700

    add CORS

--- web/server.py
+++ web/server.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 from flask import Flask
 from flask_restful import Api, Resource
-
+from flask_cors import CORS
 from address_overview import *
 from wallet_profiler import *
 from top_labels import *
@@ -13,6 +13,7 @@ from top_balances import *
 from holders import *
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Labels, "/address/<string:address>/labels", endpoint='labels')
